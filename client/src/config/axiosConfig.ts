df46ba412cb44e27df6640b5e82ed22ee8111aa9
Mubashir15700 <mubashir15700@gmail.com>
2024-06-18 11:25:31 +0530

    fix: remove hard coded base url from axios config in client

--- client/src/config/axiosConfig.ts
+++ client/src/config/axiosConfig.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-    baseURL:
-        import.meta.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api",
+    baseURL: import.meta.env.REACT_APP_API_BASE_URL,
     timeout: 10000, // 10 seconds
     headers: {
         "Content-Type": "application/json",
